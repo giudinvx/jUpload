b9f7c8f21cd205c4f20448595d769309738593dc
Giuseppe D'Inverno <giudinvx@gmail.com>
2011-02-23 04:24:41 +0100

    bug fix preview miss

--- jupload.js
+++ jupload.js
@@ -1,4 +1,24 @@
-var jUpload = function () {
+//      jupload.js
+//      
+//      Copyright 2011 Giuseppe D'Inverno <giudinvx[at]gmail[dot]com>
+//
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; either version 2 of the License, or
+//      (at your option) any later version.
+//      
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//      
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//      MA 02110-1301, USA.
+
+
+var jUpload = function() {
 	
 /*
 	if (window.FileReader) {
@@ -9,56 +29,55 @@ var jUpload = function () {
 	}
 	
 */
- 
-var dropbox = document.getElementById("dropbox");  
-dropbox.addEventListener("drop", readFiles, false );
-dropbox.addEventListener("dragleave", cancel, false);
-dropbox.addEventListener("dragover", cancel, false);
-dropbox.addEventListener("dragenter", cancel, false);
-
-function cancel (event)
-{
-	event.stopPropagation();
-	event.preventDefault();
-}
-function readFiles (event)
-{
-	event.stopPropagation();
-	event.preventDefault();
+	var dropbox = document.getElementById( "dropbox" );
 	
-	var files = event.dataTransfer.files;
-	var flength = files.length;
- 
-//    if (file[0].type.toLowerCase().match(/image.*/)) {
- 
-	for (var i = 0, file; i < flength; i++) {
-		file = files[i];
+	dropbox.addEventListener( "drop", readFiles, false );
+	dropbox.addEventListener( "dragleave", cancel, false );
+	dropbox.addEventListener( "dragover", cancel, false );
+	dropbox.addEventListener( "dragenter", cancel, false );
+
+	function cancel( event )
+	{
+		event.stopPropagation();
+		event.preventDefault();
+	}
+	
+	function readFiles( event )
+	{
+		event.stopPropagation();
+		event.preventDefault();
 		
-		var freader = new FileReader();
-			 
-		freader.onload = function (file) {
-			setFiles(file);
-		};
-
-		freader.readAsDataURL(file);
-}
-
-}
-function setFiles (file)
-{
-	var img = document.createElement('img');
-	img.file = file;
-	img.src = file.target.result;
-	preview.appendChild( img );
- 
+		var files = event.dataTransfer.files;
+		var flength = files.length;
+	 
+		for( var i = 0, file; i < flength; i++ ) {
+			file = files[i];
+			
+			if( file.type.toLowerCase().match(/image.*/) ) {
+				var freader = new FileReader();
+					 
+				freader.onload = function( file ) {
+					setFiles( file );
+				};
+				freader.readAsDataURL( file );
+			}
+		}
+	}
 	
-}
+	function setFiles( file )
+	{
+		var img = document.createElement( 'img' );
+		var preview = document.getElementById( 'preview' );
+		img.file = file;
+		img.src = file.target.result;
+		img.height = 100;
+		img.width = 150;
+		preview.appendChild( img );
+	}
 
-function resize () {
-	
-
-	
-}
-function upload () {}
+	function upload() 
+	{
+		
+	}
 
 }
