0cd002bb67a4061d76af0de4f7941de12b654183
Giuseppe D'Inverno <giudinvx@gmail.com>
2011-07-07 16:34:36 +0200

    upload func not work now

--- jupload.js
+++ jupload.js
@@ -19,22 +19,19 @@
 
 
 var jUpload = function() {
-	
-/*
-	if (window.FileReader) {
-		return true;
-	}
-	if (window.FormData) {
-		return false;
-	}
-	
-*/
+
 	var dropbox = document.getElementById( "dropbox" );
-	
+	var queue=[]; var uploas= []; 
+	var	xhr	= new XMLHttpRequest();
+
+ 
 	dropbox.addEventListener( "drop", readFiles, false );
 	dropbox.addEventListener( "dragleave", cancel, false );
 	dropbox.addEventListener( "dragover", cancel, false );
 	dropbox.addEventListener( "dragenter", cancel, false );
+
+	var uploadyes = document.getElementById( "upload" );
+	uploadyes.addEventListener( "click", upload, false );
 
 	function cancel( event )
 	{
@@ -49,13 +46,13 @@ var jUpload = function() {
 		
 		var files = event.dataTransfer.files;
 		var flength = files.length;
-	 
+
 		for( var i = 0, file; i < flength; i++ ) {
 			file = files[i];
 			
 			if( file.type.toLowerCase().match(/image.*/) ) {
 				var freader = new FileReader();
-					 
+				queue.push(file);
 				freader.onload = function( file ) {
 					setFiles( file );
 				};
@@ -66,18 +63,65 @@ var jUpload = function() {
 	
 	function setFiles( file )
 	{
-		var img = document.createElement( 'img' );
-		var preview = document.getElementById( 'preview' );
-		img.file = file;
-		img.src = file.target.result;
-		img.height = 100;
-		img.width = 150;
+		var img		= document.createElement( 'img' );
+		var preview	= document.getElementById( 'preview' );
+		img.id		= "imgpreview";
+		img.file	= file;
+		img.src		= file.target.result;
+		img.height 	= 100;
+		img.width 	= 150;
+		
 		preview.appendChild( img );
+	}
+	
+	function checkua()
+	{
+		if( window.FileReader )
+			return true;
+		
+		if( window.FormData )
+			return false;
+
+		return false;
+	}
+	
+	function generateBoundary() 
+	{
+		return "-----------------------------9" + (new Date).getTime();
+	}
+	
+	function bluildpack(file, boundary)
+	{						 
+		var mex		= '';
+ 		var	crlf	= '\r\n';
+
+		mex  = '--' + boundary + crlf;
+		mex += 'Content-Disposition: form-data; name="Files[]"; filename="'+file.name+'"'+ crlf;
+		mex += 'Content-Type: ' + file.type + crlf + crlf;
+		mex += file.getAsBinary() + crlf;
+		mex += '--' + boundary + crlf;
+		mex += '--' + boundary + '--' + crlf;
+
+		uploas.push(mex);
 	}
 
 	function upload() 
 	{
-		
-	}
+		var boundary = generateBoundary();
 
+ 		if( checkua() ) {
+ 			var leng = queue.length;
+ 
+			for( var i = 0; i < leng; i++ ) {
+				bluildpack(queue.shift(), boundary   );
+			}
+ 
+		}
+		for( var l = 0; l < uploas.length; l++ ) {
+			xhr.open("POST", "upload.php", true);
+			xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
+			xhr.sendAsBinary(uploas.shift());		 
+		}
+	}
+	
 }
---
+++ upload.php
@@ -0,0 +1,10 @@
+<?php
+ 
+print_r($_FILES);
+print_r($_POST);
+/*
+$headers = getallheaders();
+$nomefile = basename($headers['X-File-Name']);
+$streamdati = file_get_contents("php://input");
+file_put_contents($nomefile, $streamdati);
+*/
